14869569be05a5a4aeba5b457a590b0954d3bc16
NikhilRajbhar111 <nikhil35rajbhar@gmail.com>
2023-08-16 19:06:53 -0300

    Genre

--- detail.py
+++ detail.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 def scrape_category_details(title):
     # title_formatted = title.replace("-", " ").title()
     url = f"https://www4.gogoanimes.fi/category/{title}"
+    print(url)
     
     response = requests.get(url)
     if response.status_code == 200:
@@ -16,7 +17,10 @@ def scrape_category_details(title):
         anime_details = anime_detail.find_all("p")
         Name = anime_details[1].find("a").text
         Plot = anime_details[2].text.replace("Plot Summary: ", "")
-        Genre = anime_details[3].find("a").text
+        Genre_list = anime_details[3].find_all("a")
+        genre = []
+        for item in Genre_list:
+            genre.append(item["href"].replace("/genre/", ""))
         Released = anime_details[4].text.replace("Released: ", "")
         Status = anime_details[5].find("a").text
         Other_Name = anime_details[6].text.replace("Other name: ", "")
@@ -38,7 +42,7 @@ def scrape_category_details(title):
             "anime_name": anime_name,
             "Name": Name,
             "Plot": Plot,
-            "Genre": Genre,
+            "Genre": genre,
             "Released": Released,
             "Status": Status,
             "Other_Name": Other_Name,
@@ -50,3 +54,6 @@ def scrape_category_details(title):
         return anime_data
     else:
         return None
+
+data = scrape_category_details("naruto")
+print(data)
